6dbef484ac66592c43d474e88bc971ac7c641ef9
AlphaBoothes05 <73681471+AlphaBoothes05@users.noreply.github.com>
2020-11-11 19:56:27 -0700

    Merge branch 'master' of C:\Users\wero_\Documents\GitHub\SistemaTEC_APP with conflicts.

---
+++ app/src/main/java/com/example/sistematec/ui/login/Coordinator/FragmentDictumCoordinator.java
@@ -0,0 +1,64 @@
+package com.example.sistematec.ui.login.coordinator;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.sistematec.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentDictumCoordinator#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class FragmentDictumCoordinator extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentDictumCoordinator.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentDictumCoordinator newInstance(String param1, String param2) {
+        FragmentDictumCoordinator fragment = new FragmentDictumCoordinator();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public FragmentDictumCoordinator() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dictum_coordinator, container, false);
+    }
+}
