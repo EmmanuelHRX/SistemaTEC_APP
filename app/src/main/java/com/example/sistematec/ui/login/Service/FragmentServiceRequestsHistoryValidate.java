e7695167cb09868efc4223779479eb6f69c0d385
Roman Gaspar <romanalejandro99@hotmail.com>
2020-11-11 21:25:20 -0700

    modificacion a service requests history el cual ya cuenta con el tablayout y los 2 fragmentos que posteriormente contendrán las solicitudes aceptadas y rechazadas

---
+++ app/src/main/java/com/example/sistematec/ui/login/Service/FragmentServiceRequestsHistoryValidate.java
@@ -0,0 +1,24 @@
+package com.example.sistematec.ui.login.Service;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sistematec.R;
+
+public class FragmentServiceRequestsHistoryValidate extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.fragment_fragment_service_requests_history_validate, container, false);
+        setData(v);
+        return v;
+    }
+
+    private void setData(View v) {
+
+    }
+}
